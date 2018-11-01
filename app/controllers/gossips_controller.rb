6746448695002a67a650950c679778257ba4efb1
Natdenice <Clad1987@hotmail.com>
2018-11-01 09:30:14 +0100

    First commit and pushing to Heroku

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,40 @@
+class GossipsController < ApplicationController
+  
+  def index
+    @gossip = Gossip.all
+  end 
+
+  def new
+  @gossip = Gossip.new
+  end
+
+  def create
+  @gossip = Gossip.create(title: params["gossip"]["title"], content: params["gossip"]["content"], anonymous_gossiper: params["gossip"]["anonymous_gossiper"]) 
+  redirect_to @gossip
+  end
+  
+  def edit
+  @gossip = Gossip.find(params[:id])  
+  end
+
+  def show
+  @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    gossip_params = params.require(:gossip).permit(:title, :content, :anonymous_gossiper)
+    @gossip = Gossip.update(gossip_params)
+    redirect_to gossip_path
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    if @gossip.present?
+    redirect_to gossip_path
+  end  
+end
+
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/team'
+  get 'static_pages/contact'
+  resources :gossips
+  end
